635cadcd112c6dd03e9495b88e5ef88a3aafed37
Untitled71 <ethannghiem263@gmail.com>
2025-12-02 02:18:28 -0500

    Fixed vshader4x2: Slight Shading Issue
    Positional Lighting -> Directional Lighting
    
    Which matches exactly what the professor has!

--- hw4/vshader4c2.glsl
+++ hw4/vshader4c2.glsl
@@ -36,12 +36,12 @@ void main()
 	// Vector that goes from the vertex to the camera, in camera space.
 	// In camera space, the camera is at the origin (0,0,0).
 	vec3 vertexPosition_cameraspace = (u_View * vec4(a_Position, 1.0)).xyz;
-	EyeDirection_cameraspace = vec3(0.0, 0.0, 0.0) - vertexPosition_cameraspace;
+	EyeDirection_cameraspace = normalize(-(vertexPosition_cameraspace));
 
 	// Vector that goes from the vertex to the light, in camera space
 	vec3 LightPosition_cameraspace = (u_View * vec4(u_LightInvDirection, 1.0)).xyz; 
-    LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
-	
+    LightDirection_cameraspace = normalize((u_View * vec4(u_LightInvDirection, 0.0)).xyz);
+
 	// Normal of the the vertex, in camera space
 	Normal_cameraspace = (u_View * vec4(a_Normal, 0.0)).xyz;
 
